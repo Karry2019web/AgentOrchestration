be224e8ece1a21714173c7d1fcfe530ead43b996
Karry <51736839+Karry2019web@users.noreply.github.com>
2026-05-22 20:38:01 +0800

    chore: add webhook module reference

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,9 @@
 __version__ = "2.4.1"
 __name__ = "agent-orchestrator"
 
+# Webhook module
+# from src.webhook import WebhookSubscription, WebhookSubscriptionManager
+
 # 2019-02-15T15:48:45 update
 
 # 2019-04-08T20:22:08 update
